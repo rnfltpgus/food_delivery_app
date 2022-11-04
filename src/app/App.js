398d2e3e9c65a9f0f29e540a99619ec028729b00
rnfltpgus <jpp901220@gmail.com>
2022-11-04 21:39:51 +0900

    🔩 Feat, fix: 초기컴포넌트 셋팅 및 M1칩 렌딩 에러 처리

---
+++ src/app/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {MainLayout} from '../screens';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName={'Home'}>
+          <Stack.Screen name="Home" component={MainLayout} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
