e574c9164d04966d4ac84700331a36f3a4d01637
rnfltpgus <jpp901220@gmail.com>
2022-11-05 15:02:06 +0900

    🧹 style: 코드스타일 정검

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
 const Home = () => {
   return (
     <View
       style={{
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
       }}>
       <Text>Home</Text>
     </View>
